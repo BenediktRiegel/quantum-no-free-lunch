4453eed55de689140188acff654f19d9504c86c4
Benedikt Riegel <Benedikt.Riegel@gmail.com>
2022-09-01 16:47:56 +0200

    Deleted prints in readout_exp.py

--- readout_exp.py
+++ readout_exp.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    # file_paths = r'./experimental_results/std_results/result*'
-    file_paths = r'./experimental_results/std_results/result_test.txt'
+    file_paths = r'./experimental_results/std_results/result*'
+    # file_paths = r'./experimental_results/std_results/result_test.txt'
     attributes = dict(
         schmidt_rank="*",
         num_points="*",
@@ -19,8 +19,11 @@ def main():
     print(f"num of results = {len(results)}")
     preped_results = []
     for result in results:
-        print(result['losses'][-1])
-        if result['losses'][-1] == 0.0:
+        loss = result['losses']
+        if isinstance(loss[0], list):
+            loss = loss[-1]
+        # print(loss[-1])
+        if loss[-1] <= 1e-12:
             preped_results.append(result)
     # del results
     # gc.collect()
@@ -31,20 +34,17 @@ def main():
         std = np.round(res['std'], 0)
         if res['schmidt_rank'] not in unique_schmidt.keys():
             unique_schmidt[res['schmidt_rank']] = dict()
-        if unique_schmidt[res['schmidt_rank']][std] not in unique_schmidt[res['schmidt_rank']].keys():
+        if std not in unique_schmidt[res['schmidt_rank']].keys():
             unique_schmidt[res['schmidt_rank']][std] = [None]*16
-        if unique_schmidt[res['schmidt_rank']][std][res['num_points']+1] is None:
-            unique_schmidt[res['schmidt_rank']][std][res['num_points'] + 1] = []
-        unique_schmidt[res['schmidt_rank']][std][res['num_points'] + 1].append(res['risk'])
+        if unique_schmidt[res['schmidt_rank']][std][res['num_points']-1] is None:
+            unique_schmidt[res['schmidt_rank']][std][res['num_points'] - 1] = []
+        unique_schmidt[res['schmidt_rank']][std][res['num_points'] - 1].append(res['risk'])
     print(f"schmidt_ranks = {unique_schmidt.keys()}")
-    for s_rank in unique_schmidt.keys():
-        print(f"\tschmidt_ranks = {unique_schmidt.keys()} contains stds = {unique_schmidt[s_rank]}")
 
-    return
     from visualisation import generate_markers, calc_lower_bound
     num_datapoints = list(range(17))
-    r_list = [2**i for i in range(5)]
     x_qbits = 4
+    r_list = list(range(x_qbits+1))
 
     for s_rank in unique_schmidt.keys():
         tableau_palette = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown',
@@ -60,7 +60,7 @@ def main():
 
         std_idx = 0
         for std in unique_schmidt[s_rank].keys():
-            result_mean = [0] + [unique_schmidt[s_rank][std][num_points].mean() for num_points in num_datapoints[1:]]
+            result_mean = [None] + [None if unique_schmidt[s_rank][std][num_points-1] is None else np.array(unique_schmidt[s_rank][std][num_points-1]).mean() for num_points in num_datapoints[1:]]
             plt.scatter(num_datapoints, result_mean, label=f"std={std}", marker=markers[std_idx], c=tableau_palette[std_idx])
             std_idx += 1
         plt.xlabel('No. of Datapoints')
